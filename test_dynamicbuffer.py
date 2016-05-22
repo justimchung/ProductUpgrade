c81203e62158b4ba909ea82c8f52bc3b0b26e1c4
justim <justim@gmail.com>
2016-05-23 06:50:56 +0800

    加入 dynamic_buffer 的 unit test

---
+++ test_dynamicbuffer.py
@@ -0,0 +1,23 @@
+from DynamicBuffer import *
+import unittest
+
+class TestUtil(unittest.TestCase):
+    def setUp(self):
+        self.dBUffer = DynamicBuffer("int16", 10, 2)
+
+    def test_appendData(self):
+        self.dBUffer.append([10, 10])
+        self.assertEqual(len(self.dBUffer), 1)
+
+    def test_buffer_resize(self):
+        for i in range(15):
+            self.dBUffer.append([10, 10])
+
+        self.assertEqual(len(self.dBUffer), 15)
+
+    def testLen(self):
+        self.assertEqual(len(self.dBUffer), 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
