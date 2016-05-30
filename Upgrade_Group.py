7aaf38a674ce76ce5bfe5c01d5fba2c8b88e68b7
justim <justim@gmail.com>
2016-05-30 09:50:59 +0800

    Upgrade_Group 這個類別是用來支援 upgrade_algorithm 的

---
+++ Upgrade_Group.py
@@ -0,0 +1,36 @@
+from util import *
+from DynamicBuffer import DynamicBuffer
+class UpgradeGroup:
+    """
+    UpgradeGroup is a buffer that stores a set of skyline points which
+     dominate a product in the same subspace.
+     self.product is the product that is dominated by all the skyline points in the same subspace.
+     self.subspace is the subspace on which the product are worse than the skyline points
+     self.dBuffer is a buffer that store all the skyline points
+    """
+    def __init__(self, product, subspace, size, dim, datatype='uint32') :
+        self.product = product
+        self.subspace = subspace
+        self.dBuffer = DynamicBuffer(size, dim, datatype)
+
+    def addSkylinePoint(self, prod):
+        self.dBuffer.append(prod)
+
+    def getSkylineBuffer(self):
+        """
+        return the buffer that stores the skyline points
+        :return: the buffer that stores the skyline points
+        """
+        return self.dBuffer.buffer
+
+    def getSubspace(self):
+        return self.subspace
+
+    def getProduct(self):
+        return self.product
+
+
+    def sortByDim(self, dim):
+        self.dBuffer.sortByDim(dim)
+
+
