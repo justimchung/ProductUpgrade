d3341c84d3710d55f8e2f828c1a18eab367091b0
justim <justim@gmail.com>
2016-05-31 15:28:39 +0800

    完成 upgrade algorithm 的測試

--- test_upgrade_algorithms.py
+++ test_upgrade_algorithms.py
@@ -7,15 +7,17 @@ from util import *
 class TestUpgrade_algorithms(unittest.TestCase):
     def setUp(self):
         self.upG = UpgradeGroup(product = np.array([9, 8]), subspace=np.array([0, 1]), size = 5, dim = 2)
+        self.upG.addSkylinePoint(np.array([7, 8]))
+        self.upG.addSkylinePoint(np.array([8, 6]))
         self.upgradeAlg = Upgrade_Algorithm(self.upG)
 
     def test_run_upgrade_algorithm(self):
-        self.upgradeAlg.run()
+        p, minCost = self.upgradeAlg.run()
         '''after the upgrade algorithm is calling, the "product" cannot be dominated by all the
         products residing in the upG.getSkylineBuffer'''
         skyBuf = self.upG.getSkylineBuffer()
-        p = self.upG.getProduct()
         self.assertFalse(k_dom_by_points_numpy(p, 2, skyBuf))
+        self.assertEqual(3, minCost)
 
 
 if __name__ == '__main__':
