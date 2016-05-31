8c322d8dd2f5aefc0af9b43912a6f2fb9ba718f0
justim <justim@gmail.com>
2016-05-31 15:27:44 +0800

    新增 sortByDim 的方法，其可以依某個維度排序 skyline buffer 的資料
    sortByDim 是因為 upgrade algorithm 的需求所以才加入的。

--- DynamicBuffer.py
+++ DynamicBuffer.py
@@ -5,6 +5,7 @@ class DynamicBuffer():
         self.size = size
         self.length = 0
         self._buffer = np.empty((size, dim))
+        self._buffer = np.full((size, dim), np.inf)
 
     def __len__(self):
         return self.length
@@ -13,8 +14,18 @@ class DynamicBuffer():
         if self.length == self.size:
             self.size = int(1.5 * self.size)
             self._buffer.resize((self.size, self.dimension), refcheck=False)
+        self._buffer[self.length:,].fill(np.inf)
         self._buffer[self.length] = point
         self.length += 1
+
+    def sortByDim(self, dim):
+        """
+        Sort the buffer in ascending order of the dim-th dimension.
+        :param dim: the dime
+        """
+        self._buffer = self._buffer[self._buffer[:,dim].argsort()]
+
+
 
     @property
     def buffer(self):
