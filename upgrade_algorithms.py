bd395778f898f02190048ff42cd1c1838ccb2698
justim <justim@gmail.com>
2016-06-01 13:29:51 +0800

    重構 upgrade algorithm，讓程式碥更為清楚。

--- upgrade_algorithms.py
+++ upgrade_algorithms.py
@@ -4,50 +4,55 @@ from Upgrade_Group import UpgradeGroup
 class Upgrade_Algorithm:
     def __init__(self, uGroup):
         self.group = uGroup
+        #the dimensionality of the data space
+        self.dataspaceDim = len(self.group.getProduct())
 
     def run(self):
         subspace = self.group.getSubspace()
+        pOrigin = self.group.getProduct()
+        pMinCost = None
+        minCost = np.inf
 
-        p = self.group.getProduct()
+        for currentDim in np.nditer(subspace):
 
-        pMinCost = np.copy(p)
-
-        cost = np.inf
-
-        for dim in range(len(subspace)):
-
-            ptmp = np.copy(p)
-
-            self.group.sortByDim(dim)
+            self.group.sortByDim(currentDim)
 
             skyBuffer = self.group.getSkylineBuffer()
 
-            dimMinValue = skyBuffer[0][dim] - 1
-            ptmp[dim] = dimMinValue
+            ptmp = self.__upgradeProductInOneDim(currentDim, pOrigin, skyBuffer)
 
-            ugCost = util.getCost_numpy(ptmp, p)
-            if ugCost < cost:
-                cost = ugCost
-                pMinCost = ptmp
-
-            ptmp2 = np.copy(p)
+            minCost, pMinCost = self.__determineMinCostProduct(minCost, pOrigin, pMinCost, ptmp)
 
             for j in range(len(skyBuffer) - 1):
-                si = skyBuffer[j]
-                sj = skyBuffer[j + 1]
+                ptmp = self.__upgradeProductMultipDim(currentDim, j, skyBuffer, subspace)
 
-                for k in range(len(subspace)):
-                    if k == dim:
-                        ptmp2[k] = sj[k] - 1
-                    else:
-                        ptmp2[k] = si[k] - 1
+                minCost, pMinCost = self.__determineMinCostProduct(minCost, pOrigin, pMinCost, ptmp)
 
-                ugCost = util.getCost_numpy(ptmp2, p)
-                if ugCost < cost:
-                    cost = ugCost
-                    pMinCost = ptmp2
+        return pMinCost, minCost
 
-        return pMinCost, cost
+    def __upgradeProductMultipDim(self, currentDim, itemIndex, skyBuffer, subspace):
+        ptmp2 = np.zeros(self.dataspaceDim)
+        si = skyBuffer[itemIndex]
+        sj = skyBuffer[itemIndex + 1]
+        for k in range(len(subspace)):
+            if k == currentDim:
+                ptmp2[k] = sj[k] - 1
+            else:
+                ptmp2[k] = si[k] - 1
+        return ptmp2
+
+    def __upgradeProductInOneDim(self, dim, p, skyBuffer):
+        ptmp = np.copy(p)
+        dimMinValue = skyBuffer[0][dim] - 1
+        ptmp[dim] = dimMinValue
+        return ptmp
+
+    def __determineMinCostProduct(self, currentMinCost, p, pMinCost, ptmp):
+        ugCost = util.getCost_numpy(ptmp, p)
+        if ugCost < currentMinCost:
+            currentMinCost = ugCost
+            pMinCost = ptmp
+        return currentMinCost, pMinCost
 
 
 
