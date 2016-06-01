c5cd013cc2df31af8f9a1701a3d0d5c285045fad
justim <justim@gmail.com>
2016-06-01 13:32:09 +0800

    新增 test_run_upgrade_alg_for_one_dominantsky 測試函式
    test_run_upgrade_alg_for_one_dominantsky 函式會測試 upgrade algorithm 是否在 one-dominant skyline 的狀況下也可以正常運作。

--- test_upgrade_algorithms.py
+++ test_upgrade_algorithms.py
@@ -5,13 +5,12 @@ import numpy as np
 from util import *
 
 class TestUpgrade_algorithms(unittest.TestCase):
-    def setUp(self):
+
+    def test_run_upgrade_algorithm_for_full_skyline(self):
         self.upG = UpgradeGroup(product = np.array([9, 8]), subspace=np.array([0, 1]), size = 5, dim = 2)
         self.upG.addSkylinePoint(np.array([7, 8]))
         self.upG.addSkylinePoint(np.array([8, 6]))
         self.upgradeAlg = Upgrade_Algorithm(self.upG)
-
-    def test_run_upgrade_algorithm(self):
         p, minCost = self.upgradeAlg.run()
         '''after the upgrade algorithm is calling, the "product" cannot be dominated by all the
         products residing in the upG.getSkylineBuffer'''
@@ -19,6 +18,18 @@ class TestUpgrade_algorithms(unittest.TestCase):
         self.assertFalse(k_dom_by_points_numpy(p, 2, skyBuf))
         self.assertEqual(3, minCost)
 
+    def test_run_upgrade_alg_for_one_dominantsky(self):
+        """
+        test_run_upgrade_alg_for_one_dominantsky test whether the upgrade algorithm can
+        work well in 1-dominant skyline scenario
+        """
+        self.upG = UpgradeGroup(product = np.array([6, 8]), subspace=np.array([1]), size = 5, dim = 2)
+        self.upG.addSkylinePoint(np.array([8, 6]))
+        self.upgradeAlg = Upgrade_Algorithm(self.upG)
+        p, minCost = self.upgradeAlg.run()
+        skyBuf = self.upG.getSkylineBuffer()
+        self.assertFalse(k_dom_by_points_numpy(p, 1, skyBuf))
+        self.assertEqual(3, minCost)
 
 if __name__ == '__main__':
     unittest.main()
