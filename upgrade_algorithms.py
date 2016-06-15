cc4451e826564ad094336ce7bfcf234891b6224a
justim <justim@gmail.com>
2016-06-15 16:34:01 +0800

    新增 newUpgradeAlgorithm
    New_Upgrade_Algorihtm 會呼叫 Upgrade Algorithm 來做 k-dom skyline 產品的升級。目前完成部分功能，但還沒有測試。

--- upgrade_algorithms.py
+++ upgrade_algorithms.py
@@ -1,11 +1,13 @@
+# -*-coding: utf-8 -*-
 import numpy as np
 import util
+from collections import deque
 from Upgrade_Group import UpgradeGroup
 class Upgrade_Algorithm:
     def __init__(self, uGroup):
         self.group = uGroup
         #the dimensionality of the data space
-        self.dataspaceDim = len(self.group.getProduct())
+        self.dataspaceDim = uGroup.getDIM()
 
     def run(self):
         subspace = self.group.getSubspace()
@@ -54,14 +56,54 @@ class Upgrade_Algorithm:
             pMinCost = ptmp
         return currentMinCost, pMinCost
 
+
+
+
 class New_Upgrade_Algorithm:
-    def __init__(self, uGroup, kValue):
+    def __init__(self, uGroup, kValue, orgP):
         self.upgradeGroup = uGroup
         self.upgAlg = Upgrade_Algorithm(self.upgradeGroup)
-        self.upgradeGroup.kValue = kValue
+        self.kValue = kValue
+        self.orgP = orgP
+        self.minValBuf = np.full(self.upgradeGroup.getDIM(), np.inf)
+        self.dim = uGroup.getDIM()
+
+    def run(self):
+        queue = deque()
+        skyBuf = self.upgradeGroup.getSkylineBuffer()
+        queue.append(self.upgradeGroup)
+        while(len(queue) != 0):
+            UG = queue.pop()
+            upgAlg = Upgrade_Algorithm(UG)
+            p, cost = upgAlg.run()
+            if util.k_dom_by_points_numpy(p, self.kValue, skyBuf) == True:
+                self.__classify_points_by_product(p, UG, queue)
+            else:
+                self.__modify_minValueBuf(p)
 
 
+    def __modify_minValueBuf(self, p):
+        self.minValBuf = np.minimum(p, self.minValBuf)
 
-
+    def __classify_points_by_product(self, p, UG, queue):
+        """
+        依照 p 將 UG 中的每個元素分類，例如分成在 1,2 維度 dominate 一類，
+        在 2, 3維度的一類這樣。
+        :param p: A product
+        :param UG: the upgrade group that will be decomposed
+        :param queue: the new upgrade group
+        :return:
+        """
+        dict = {}
+        skyBuf = UG.getSkylineBuffer()
+        for skyP in skyBuf:
+            subSpace = util.getDominateSubspace_numpy(p, skyP)
+            id = util.getSubspaceUniqueID_numpy(subSpace)
+            if dict.has_key(id):
+                dict[id].addSkylinePoint(skyP)
+            else:
+                dict[id] = UpgradeGroup(self.orgP, subspace=subSpace, size = 25, dim = self.dim)
+        for newUG in dict.values():
+            queue.append(newUG)
 
 
