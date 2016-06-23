2e1ac2d5d45a64b8f02174c1bba6707818c4a843
justim <justim@gmail.com>
2016-06-23 16:16:37 +0800

    Refactor 變數的名字
    將變數的拚法改成正確的拚法

--- test_dynamicbuffer.py
+++ test_dynamicbuffer.py
@@ -3,28 +3,28 @@ import unittest
 
 class TestUtil(unittest.TestCase):
     def setUp(self):
-        self.dBUffer = DynamicBuffer(10, 2)
+        self.dBuffer = DynamicBuffer(10, 2)
 
     def test_appendData(self):
-        self.dBUffer.append([10, 10])
-        self.assertEqual(len(self.dBUffer), 1)
+        self.dBuffer.append([10, 10])
+        self.assertEqual(len(self.dBuffer), 1)
 
     def test_buffer_resize(self):
         for i in range(15):
-            self.dBUffer.append([10, 10])
+            self.dBuffer.append([10, 10])
 
-        self.assertEqual(len(self.dBUffer), 15)
+        self.assertEqual(len(self.dBuffer), 15)
 
     def testLen(self):
-        self.assertEqual(len(self.dBUffer), 0)
+        self.assertEqual(len(self.dBuffer), 0)
 
     def test_sortByDim(self):
-        self.dBUffer.append([1,2])
-        self.dBUffer.append([4,1])
-        self.dBUffer.append([0,0])
-        self.dBUffer.sortByDim(1)
-        self.assertEqual(self.dBUffer.buffer[0][0], 0)
-        self.assertEqual(self.dBUffer.buffer[2][0], 1)
+        self.dBuffer.append([1, 2])
+        self.dBuffer.append([4, 1])
+        self.dBuffer.append([0, 0])
+        self.dBuffer.sortByDim(1)
+        self.assertEqual(self.dBuffer.buffer[0][0], 0)
+        self.assertEqual(self.dBuffer.buffer[2][0], 1)
 
 if __name__ == '__main__':
     unittest.main()
