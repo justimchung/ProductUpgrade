29df686634c5f23d0a91c521f415188306072255
justimchung <justim@gmail.com>
2016-07-05 06:50:04 +0800

    對 simulator 做許多的更新，加入測試時間，依據演算法的名字初始化不同的 algorithm

--- simulator.py
+++ simulator.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 import numpy as np
+import time
+
 import util
 import Upgrade_Group as ug
 import upgrade_algorithms as ualg
@@ -10,19 +12,33 @@ class simulator:
         self.skyBuffer = self.__retriveSkylines()
         self.kValue = kwargs['kValue']
         self.product = kwargs['product']
+        self.algName = kwargs['algName']
         self.dim = len(self.product)
+        self.upGroup = self.__initialUpgradeGroup()
         self.upAlg = self.__initialAlgorithm()
 
     def __retriveSkylines(self):
         return util.retrieveSkylinePoints_numpy(self.rawDataBuffer)
 
-    def __initialAlgorithm(self):
+    def __initialUpgradeGroup(self):
         aUG = ug.UpgradeGroup(self.product, np.arange(self.dim), size=1000, dim=self.dim)
         aUG.addSkylinePoints(self.skyBuffer)
-        upgradeAlg = ualg.Upgrade_Algorithm(aUG)
+        return aUG
+
+    def __initialAlgorithm(self):
+        if self.algName == 'Upgrade_Algorithm':
+            upgradeAlg = ualg.Upgrade_Algorithm(self.upGroup)
+        elif self.algName == 'New_Upgrade_Algorithm':
+            upgradeAlg = ualg.New_Upgrade_Algorithm(self.upGroup, self.kValue)
         return upgradeAlg
 
-
     def run(self):
+        start_time = time.time()
         self.upAlg.run()
+        end_time = time.time()
+        secs = end_time - start_time
+        print(secs)
 
+if __name__ == '__main__':
+    sim = simulator(kValue=2, product=np.array([500, 500, 500]), fileName='data_uni_20_3_0.db', algName='Upgrade_Algorithm')
+    sim.run()
