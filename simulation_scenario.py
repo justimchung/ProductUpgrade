03556efe90a2e41ad16591d1f36ef4b863a3b55f
justim <justim@gmail.com>
2016-07-19 17:11:06 +0800

    加入 logging 的功能，可以記錄執行的結果

--- simulation_scenario.py
+++ simulation_scenario.py
@@ -1,31 +1,68 @@
 import util
 import numpy as np
 import simulator as sim
+import logging
+import logging.handlers
+import os
 
-numIteration = 10 #number of iterations for each test
+numIteration = 5 #number of iterations for each test
 defaultDim = 11
 defaultDataSize = 100
-datasizeIndex = 0
-dimIndex = 1
-kValueIndex = 2
+datasizeIndex = 1
+dimIndex = 2
+kValueIndex = 4
 distIndex = 3
-algIndex = 4
+algIndex = 0
+SimulationLogFileName = 'Product_Upgrade_Sim_log.log'
+LoggerName = 'upgrade_product_logger'
 
+def getCurrentPath():
+    return os.path.dirname(os.path.abspath(__file__))
 
-def getDataFileName(dist, dim, iter):
-    fname = 'data_%s_%d_%d_%d.db' % (dist, dim, iter)
+def getDataFileName(dist, datasize, dim, iter):
+    fname = getCurrentPath() + '\data\data_%s_%d_%d_%d.db' % (dist, datasize, dim, iter)
     return fname
 
+def getProductFileName(dim):
+    fname = getCurrentPath() + '\data\product_uni_1_%d_0.db' % (dim)
+    return fname
 
 def effect_k_scenario():
+    logger = logging.getLogger('upgrade_product_logger')
+    logger.debug('Effect of K simulation')
     kvalues = [6, 7, 8, 9, 10]
     dims = np.full(1, defaultDim, dtype='int32')
     datasizes = np.full(1, defaultDataSize, dtype='int32')
     dists = np.array(['corr', 'uni', 'anti'])
     algNames = np.array(['Upgrade_Algorithm', 'New_Upgrade_Algorithm'])
-    simSettings = util.cartesian(datasizes, dims, kvalues, dists, algNames)
+    simSettings = generate_simulation_settings(algNames, datasizes, dims, dists, kvalues)
     for setting in simSettings:
+        ProductFileName = getProductFileName(int(setting[dimIndex]))
+        ProductBuffer = np.loadtxt(ProductFileName, delimiter=',')
+        ProductIndex = 0
         for iter in range(numIteration):
-            fileName = getDataFileName(setting[distIndex], setting[dimIndex], iter)
-            sim.simulator(kValue=setting[kValueIndex], fileName=fileName, algName=setting[algIndex], product=np.array([10,10,10]))
+            logger.debug('[info]Dist = %s, dataSize = %d, dim = %d, k = %d, iter = %d' %
+                         (setting[distIndex], int(setting[datasizeIndex]), int(setting[dimIndex]),
+                                                                              int(setting[kValueIndex]), iter))
+            DataFileName = getDataFileName(setting[distIndex], int(setting[datasizeIndex]), int(setting[dimIndex]), iter)
+            aSim = sim.simulator(kValue=int(setting[kValueIndex]), fileName=DataFileName, algName=setting[algIndex],
+                          product=ProductBuffer[ProductIndex])
+            aSim.run()
+            ProductIndex += 1
 
+
+def generate_simulation_settings(algNames, datasizes, dims, dists, kvalues):
+    simSettings = util.cartesian((algNames, datasizes, dims, dists, kvalues))
+    return simSettings
+
+
+def initial_logging():
+    logger = logging.getLogger(LoggerName)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.handlers.RotatingFileHandler(SimulationLogFileName)
+    logger.addHandler(handler)
+
+
+if __name__ == '__main__':
+    initial_logging()
+    effect_k_scenario()
