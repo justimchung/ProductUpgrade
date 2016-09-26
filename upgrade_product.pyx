6d13a089b5969a0c31bec6c7a0f55bd6c5ea71fc
justim <justim@gmail.com>
2016-09-26 09:44:25 +0800

    upgrade_product 代表的是一個 product，這個類別將用於 upgrade algorithm 中
    upgProduct 代表一個 product，使用 product 的原因是之後需要使用 queue 來儲存一群 product，有一個 class，這樣比較好儲存資料。

---
+++ upgrade_product.pyx
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+# distutils: language = c++
+# distutils: sources = Source.cpp
+# use python cython_setup.py build_ext --inplace to complier the source code
+import numpy as np
+from cython cimport boundscheck, wraparound
+cimport numpy as np
+
+DTYPE = np.int
+ctypedef np.int_t DTYPE_t
+
+cdef class upgProduct:
+    """
+    The upgraded product class. The class is used in the upgrade algorithm.
+    self.attributes is the attribute list of the product
+    self.getUpgCost returns the upgrade cost of the product
+    """
+    cdef public int[::1] attributes
+    cdef double upgCost
+    def __init__(self, int[::1] attri):
+        self.attributes = attri
+        self.upgCost = 0.0
+    cdef inline __setCost(self, double aCost):
+        self.upgCost = aCost
+    cdef inline double __getCost(self):
+        return self.upgCost
+    def setCost(self, cost):
+        self.__setCost(cost)
+    def getCost(self):
+        return self.__getCost()
